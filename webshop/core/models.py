80b7efeb570504cb01edb3acabc8cdfbaae47832
markossify95 <markoss95@hotmail.com>
2017-02-23 04:00:50 +0100

    started orm development

--- webshop/core/models.py
+++ webshop/core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Proizvod(models.Model):
+    marka = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    cena = models.FloatField()
+    kolicina_na_stanju = models.PositiveIntegerField(default=0)
+    opis = models.TextField()
+
+class Sporet(models.Model):
+    proizvod = models.ForeignKey('Proizvod', on_delete=models.CASCADE)
+    TIPOVI = (
+        ('RP', 'Ravna Ploca'),
+        ('ST', 'Standardni')
+    )
+    tip = models.CharField(max_length=2, choices = TIPOVI, default='ST')
+    ostalo = models.TextField()
